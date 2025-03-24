d504542e019a7041d94f3a3a6d58350bd3a38d69
samndambuki <sam.ndambuki08@gmail.com>
2025-03-24 14:09:20 +0300

    worked on data binding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 export const routes: Routes = [
   { path: '', component: HelloComponent },
   { path: 'hello', component: HelloComponent },
+  { path: 'data-binding', component: DataBindingComponent },
 ];
--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
-  styleUrl: './data-binding.component.css'
+  styleUrl: './data-binding.component.css',
 })
 export class DataBindingComponent {
-
+  //types of data binding
+  //1. on way data binding (from component to view)
+  //a) string interpolation
+  username = 'Samuel Ndambuki';
+  //b) property binding
+  profilepic = 'profile.jpg';
+  //c)atrribute,class and style binding
+  isDisabled?: true;
+  label = 'enter your name';
+  isError = true;
+  bgcolor = 'blue';
+  //2. two way data binding - from view to component
+  //a)event binding
+  sayHello() {
+    alert('Hello!');
+  }
 }
